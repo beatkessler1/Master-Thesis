9946d08eb4eecee0921c7892e10dd47a65139bca
Beat Kessler <100423150+beatkessler1@users.noreply.github.com>
2025-09-04 09:23:20 +0200

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,17 +1,41 @@
-# -*- coding: utf-8 -*-
 import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
 
+from office365.runtime.auth.user_credential import UserCredential
+from office365.sharepoint.client_context import ClientContext
 
+from utils import getAuthSession, get_files_in_folder
+        
 
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+base_url = 'https://collaboration.merck.com'
+site_path = '/sites/SCM-Learning'
 
-selected_files_df = ... # Compute a Pandas dataframe to write into selected_files
+username, password = getAuthSession(kerberos=False)
 
+user_credentials = UserCredential(f'{username}@merck.com', password)
+ctx = ClientContext(base_url + site_path).with_credentials(user_credentials)
 
-# Write recipe outputs
-selected_files = dataiku.Dataset("selected_files")
-selected_files.write_with_schema(selected_files_df)
+output_folder = dataiku.Folder('a9WV0Cop')
+output_path = output_folder.get_path()
+
+folder_path = site_path + '/Shared Documents'
+
+suffix_count = dict()
+error_files = list()
+
+for file_path in get_files_in_folder(ctx, folder_path):
+    file_name = os.path.basename(file_path)
+    relative_path = os.path.relpath(file_path, folder_path)
+    
+    # Avoid downloading Excel files
+    _, file_extension = os.path.splitext(file_name)
+    
+    if file_extension not in ['.pptx', '.docx', '.pdf', '.xlsx', '.xls', '.xlsm']:
+        print(f'Skipping file {relative_path} as it is not of the required type.')
+        continue
+    
+    # Create the full directory path locally
+    local_dir = os.path.dirname(relative_path)
+    local_dir_full = os.path.join(output_path, local_dir)
+    
+    os.makedirs(local_dir_full, exist_ok=True)
+
